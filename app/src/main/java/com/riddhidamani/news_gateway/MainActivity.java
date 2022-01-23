ef9acf488421795afa9415b8928821c24c25190a
Riddhi Damani <riddhidamani90@gmail.com>
2022-01-22 20:20:59 -0600

    Adding Layout files

--- app/src/main/java/com/riddhidamani/news_gateway/MainActivity.java
+++ app/src/main/java/com/riddhidamani/news_gateway/MainActivity.java
@@ -1,14 +1,38 @@
 package com.riddhidamani.news_gateway;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    // Drawer variables
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    // Menu variables
+    private Menu opt_menu;
+
+    // ViewPager variables
+    private ViewPager2 viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Make sample items for menu
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerList = findViewById(R.id.left_drawer_list);
+
     }
 }
